86e01dcb653ea00e066f50ac5998d70b85e4d83e
ED <stevencaro30@gmail.com>
2024-04-08 20:44:50 -0500

    Fixed api auth

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,18 +1,7 @@
 import { NextResponse, NextRequest } from "next/server"
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import mysql from 'mysql2/promise'
-import dotenv from 'dotenv'
 const saltRounds = 10
-
-dotenv.config()
-
-const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-}
 
 export async function GET(request: NextRequest) {
 
@@ -32,35 +21,35 @@ export async function POST(request: Request) {
 
   try {
     // Conectar a la base de datos
-    const connection = await mysql.createConnection(dbConfig);
+    // // const connection = await mysql.createConnection(dbConfig);
 
-    // Consultar el usuario por email
-    const [rows]: any = await connection.execute('SELECT id, user, password FROM users WHERE user = ?', [user])
-    const getUser = rows[0]
+    // // // Consultar el usuario por email
+    // // const [rows]: any = await connection.execute('SELECT id, user, password FROM users WHERE user = ?', [user])
+    // // const getUser = rows[0]
 
-    console.log(getUser)
+    // // console.log(getUser)
 
-    if (!getUser) {
-      return NextResponse.json({ message: 'Invalid credentials.' })
-    }
+    // // if (!getUser) {
+    // //   return NextResponse.json({ message: 'Invalid credentials.' })
+    // // }
 
-    // Verificar la contraseña
-    const encryptPassword = bcrypt.hashSync(pass, saltRounds)
-    console.log(encryptPassword)
+    // // // Verificar la contraseña
+    // // const encryptPassword = bcrypt.hashSync(pass, saltRounds)
+    // // console.log(encryptPassword)
 
-    console.log(getUser.password)
-    const isValidPassword = await bcrypt.compare(pass, getUser.password)
+    // // console.log(getUser.password)
+    // // const isValidPassword = await bcrypt.compare(pass, getUser.password)
 
-    console.log(isValidPassword)
+    // // console.log(isValidPassword)
 
-    if (!isValidPassword) {
-      return NextResponse.json({ message: 'Invalid credentials.' })
-    }
+    // // if (!isValidPassword) {
+    // //   return NextResponse.json({ message: 'Invalid credentials.' })
+    // // }
 
-    // Generar un token JWT
-    const token = jwt.sign({ id: getUser.id, user: getUser.user }, JWT_SECRET, { expiresIn: '24h' })
+    // // Generar un token JWT
+    // const token = jwt.sign({ id: getUser.id, user: getUser.user }, JWT_SECRET, { expiresIn: '24h' })
 
-    return NextResponse.json({ status: 200, token })
+    // return NextResponse.json({ status: 200, token })
   } catch (error: any) {
     console.error('Error de conexión a la base de datos:', error);
     return NextResponse.json({ status: 400, message: 'Error de conexión a la base de datos.' });
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,8 @@
 'use client'
-import {NextUIProvider} from '@nextui-org/react';
-import {useRouter} from 'next/navigation'
+import { NextUIProvider } from '@nextui-org/react';
+import { useRouter } from 'next/navigation'
 
-export function Providers({children}: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
   return (
