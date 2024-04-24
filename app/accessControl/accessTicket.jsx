3dfd8e62a1c70ef6a9776e9b68c7564868e3accd
ED <stevencaro30@gmail.com>
2024-04-23 21:11:46 -0500

    Edit ticket

--- app/accessControl/accessTicket.jsx
+++ app/accessControl/accessTicket.jsx
@@ -1,23 +1,33 @@
 "use client"
 import React, { useEffect, useRef } from 'react'
 import { useReactToPrint } from 'react-to-print'
-import { Divider, Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react"
+import { Divider, Card, CardHeader, CardBody, CardFooter, Image } from "@nextui-org/react"
 
 
-const ComponentToPrint = React.forwardRef(({ visitor, address, getDate, by }, ref) => {
+const ComponentToPrint = React.forwardRef(({ visitor, address, numTag, condominium, getDate, by }, ref) => {
     return (
         <div ref={ref} className="text-center w-full">
             <Card>
-                <CardHeader className="flex justify-center text-center">
-                    <p className='text-5xl font-bold my-10'>Visitor Pass</p>
+                <CardHeader className="flex text-center justify-between">
+                    <Image
+                        className="w-40 h-auto mr-2"
+                        src="/bluelogo.png"
+                        alt="Login icon"
+                        width={240}
+                        height={240}
+                    />
+                    <p className='text-4xl font-bold max-w-2xl'>{condominium}</p>
+                    <p className='text-2xl font-bold'>Visitor <br /> Pass</p>
                 </CardHeader>
                 <Divider />
                 <CardBody className='mx-14'>
-                    <p className='text-3xl font-bold my-10'>Visitor Name: <span className='font-normal'> {visitor} </span> </p>
+                    <p className='text-3xl font-bold my-4'>Visitor Name: <span className='font-normal'> {visitor} </span> </p>
 
-                    <p className='text-3xl font-bold my-10'>Address: <span className='font-normal'> {address} </span> </p>
+                    <p className='text-3xl font-bold my-4'>Address: <span className='font-normal'> {address} </span> </p>
 
-                    <p className='text-3xl font-bold my-10'>Time: <span className='font-normal'> {getDate} </span> </p>
+                    <p className='text-3xl font-bold my-4'>Tag Number: <span className='font-normal'> {numTag} </span> </p>
+
+                    <p className='text-3xl font-bold my-4'>Time: <span className='font-normal'> {getDate} </span> </p>
                 </CardBody>
                 <Divider />
                 <CardFooter className='flex flex-col items-start text-left'>
@@ -32,7 +42,7 @@ const ComponentToPrint = React.forwardRef(({ visitor, address, getDate, by }, re
 
 ComponentToPrint.displayName = "ComponentToPrint"
 
-const AccessTicket = ({ printTicket, setPrintTicket, visitor, address, getDate, by }) => {
+const AccessTicket = ({ printTicket, setPrintTicket, visitor, address, numTag, condominium, getDate, by }) => {
     const componentRef = useRef()
     const handlePrint = useReactToPrint({
         content: () => componentRef.current,
@@ -46,8 +56,8 @@ const AccessTicket = ({ printTicket, setPrintTicket, visitor, address, getDate, 
     }, [printTicket, handlePrint, setPrintTicket])
 
     return (
-        <div className='hidden'>
-            <ComponentToPrint ref={componentRef} visitor={visitor} address={address} getDate={getDate} by={by} />
+        <div className=''>
+            <ComponentToPrint ref={componentRef} visitor={visitor} address={address} numTag={numTag} condominium={condominium} getDate={getDate} by={by} />
         </div>
     )
 }
